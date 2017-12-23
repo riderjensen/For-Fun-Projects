bc0d95beeb38e094810a6a4b0169412fabdac990
Unknown <riderjensen1@gmail.com>
2017-12-23 14:56:38 -0700

    Deleteing some garbage, trying to get PVP area status working

--- Character Search/js/main.js
+++ Character Search/js/main.js
@@ -2,14 +2,13 @@
 "use strict()";
 
 var realmName;
-var charName;
 
 //Getting realm names
 var askRealmList = new XMLHttpRequest();
 
 function blizzAPI(){
-
-		var requestURL = "https://us.api.battle.net/wow/character/" + realmName + "/" + charName + "?locale=en_US&apikey=ez9mqbh86f3sugau2fg4anumnjk67zmp";
+		var requestURL = "https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=ez9mqbh86f3sugau2fg4anumnjk67zmp";
+		//var requestURL = "https://us.api.battle.net/wow/"+ realmName+"/status?locale=en_US&apikey=ez9mqbh86f3sugau2fg4anumnjk67zmp";
 		askRealmList.open('GET', requestURL, true);
 		askRealmList.responseType = "text";
 		askRealmList.send(null);
@@ -19,33 +18,17 @@ askRealmList.onload = function(){
 		if (askRealmList.status == 200) {
 			rObj = JSON.parse(askRealmList.responseText);
 			console.log(rObj);
-
-			//create row element
-			var rowDiv = document.createElement("div");
-			rowDiv.setAttribute("class", "row");
-			//create column 1 tag
-			var col1Div = document.createElement("div");
-			col1Div.setAttribute("class", "col-lg-6");
-			//create column 2 tag
-			var col2Div = document.createElement("div");
-			col2Div.setAttribute("class", "col-lg-6");
-
-			var paraTag = document.createElement("p");
-			var paraTextNode = document.createTextNode(rObj.name);
-
-			paraTag.appendChild(paraTextNode);
-			col1Div.appendChild(paraTag);
-			rowDiv.appendChild(col1Div);
+			for (var i = 0; i < rObj.realms.length; i++) {
+				var money = rObj.realms[i].next;
+				console.log(money);
+			}
 			
-			var element = document.getElementById("dataDump");
-			element.appendChild(rowDiv);
 
 		}
 };
 
 
 function main(){
-	charName = document.getElementById("charName").value;
 	realmName = document.getElementById("realmName").value;
 	blizzAPI();
 
