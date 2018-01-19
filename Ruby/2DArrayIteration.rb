89422e6fbab121f1a927e483a2a3896c7dcb2db8
Unknown <riderjensen1@gmail.com>
2018-01-18 17:48:33 -0700

    Arrays, Hashes, And Looping Over Them

---
+++ Ruby/2DArrayIteration.rb
@@ -0,0 +1,2 @@
+s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+s.each{|sub_array| sub_array.each {|sub_sub_array| puts sub_sub_array}}
---
+++ Ruby/hashLooping.rb
@@ -0,0 +1,23 @@
+secret_identities = {
+  "The Batman" => "Bruce Wayne",
+  "Superman" => "Clark Kent",
+  "Wonder Woman" => "Diana Prince",
+  "Freakazoid" => "Dexter Douglas"
+}
+  
+secret_identities.each do |superhero, real_name|
+	puts "#{superhero}: #{real_name}"
+end
+
+#only print one pair
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+
+lunch_order.each do |element1, element2|
+  puts "#{element2}"
+end
