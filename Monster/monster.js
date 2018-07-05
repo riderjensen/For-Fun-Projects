d2d2f99dc52835f2bde7e3d11de2b73e867296e2
Unknown <riderjensen@gmail.com>
2018-07-05 08:58:41 -0600

    Working on codepen, issues on attacking

--- Monster/monster.js
+++ Monster/monster.js
@@ -9,61 +9,94 @@ class Monster{
         this.intelligence = intelligence;
     }
     run(){
-
+        if (getIntMinMax(1, 2) === 1){
+            console.log('You ran!')
+            return true;
+        }
+        else{
+            console.log("You couldn't get away!")
+            return false;
+        }
+    }
+    giveExp(){
+        return (this.speed + this.strength + this.agility + this.wisdom + this.intelligence) * (0.2*this.health);
     }
 }
 class Flying extends Monster{
-    constructor(name, health, speed, strength, agility, wisdom, intelligence){
-        super(name, health, speed, strength, agility, wisdom, intelligence);
+    constructor(name){
+        super(name,  getIntMinMax(50, 100),  getIntMinMax(5, 10),  getIntMinMax(1, 6),  getIntMinMax(2, 7),  getIntMinMax(1, 5),  getIntMinMax(3, 8));
     }
-    attack(){
-        
+    attack(playerArmor){
+        return (this.speed*3)-(playerArmor*2);
     }
     defend(){
-
+        return this.strength*getIntMinMax(1, 4);
     }
 }
 class Fighter extends Monster{
-    constructor(name, health, speed, strength, agility, wisdom, intelligence){
-        super(name, health, speed, strength, agility, wisdom, intelligence);
+    constructor(name){
+        super(name,  getIntMinMax(85, 100),  getIntMinMax(2, 7),  getIntMinMax(5, 10),  getIntMinMax(1, 5),  getIntMinMax(1, 6),  getIntMinMax(3, 8));
     }
-    attack(){
-
+    attack(playerArmor){
+        return (this.strength*3)-(playerArmor*2);
     }
     defend(){
-
+        return this.wisdom*getIntMinMax(1, 4);
     }
 }
 class Rogue extends Monster{
-    constructor(name, health, speed, strength, agility, wisdom, intelligence){
-        super(name, health, speed, strength, agility, wisdom, intelligence);
+    constructor(name){
+        super(name,  getIntMinMax(60, 85),  getIntMinMax(1, 6),  getIntMinMax(3, 8),  getIntMinMax(5, 10),  getIntMinMax(1, 5),  getIntMinMax(2, 7));
     }
-    attack(){
-
+    attack(playerArmor){
+        return (this.agility*3)-(playerArmor*2);
     }
     defend(){
-
+        return this.speed*getIntMinMax(1, 4);
     }
 }
 class Shaman extends Monster{
-    constructor(name, health, speed, strength, agility, wisdom, intelligence){
-        super(name, health, speed, strength, agility, wisdom, intelligence);
+    constructor(name){
+        super(name,  getIntMinMax(50, 80),  getIntMinMax(1, 5),  getIntMinMax(2, 7),  getIntMinMax(3, 8),  getIntMinMax(5, 10),  getIntMinMax(1, 6));
     }
-    attack(){
-  
+    attack(playerArmor){
+        return (this.wisdom*3)-(playerArmor*2);
     }
     defend(){
-  
+        return this.intelligence*getIntMinMax(1, 4);
     }
 }
 class Wizard extends Monster{
-    constructor(name, health, speed, strength, agility, wisdom, intelligence){
-        super(name, health, speed, strength, agility, wisdom, intelligence);
+    constructor(name){
+        super(name,  getIntMinMax(50, 70),  getIntMinMax(3, 8),  getIntMinMax(1, 5),  getIntMinMax(1, 6),  getIntMinMax(2, 7),  getIntMinMax(5, 10));
     }
-    attack(){
-  
+    attack(playerArmor){
+        return (this.intelligence*3)-(playerArmor*2);
     }
     defend(){
-  
+        return this.agility*getIntMinMax(1, 4);
     }
 }
+
+
+class Player extends Monster{
+    constructor(name, health, speed, strength, agility, wisdom, intelligence, armor, mainStat){
+        super(name, health, speed, strength, agility, wisdom, intelligence);
+        this.armor = armor;
+        this.mainStat = this.mainStat;
+    }
+    attack(mainStat){
+        return (mainStat * getIntMinMax(1, 4 * 0.5));
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+function getIntMinMax(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
